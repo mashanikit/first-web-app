9e955bfc4711e678c809d35145f255aaacacc41c
MASHA <mashanikit@gmail.com>
2017-06-02 16:40:10 -0400

    Finished exercise

--- server.rb
+++ server.rb
@@ -2,13 +2,27 @@ require 'sinatra'
 
 get '/home' do
   erb :index
-
 end
 
-get '/gallery' do
-  erb :gallery
+get '/portfolio' do
+  erb :portfolio
 end
 
 get '/about_me' do
+  @skills = ['git', 'HTML', 'CSS', 'Ruby']
+  @interests = ['dogs', 'art', 'music', 'films', 'coffee']
   erb :about_me
+end
+
+get '/favourites' do
+  @favourites = ['reddit', 'facebook', 'soundcloud', 'youtube', 'alexa']
+  erb :favourites
+end
+
+get '/' do
+  redirect to('/home')
+end
+
+get '/gallery' do
+  redirect to('/portfolio')
 end
